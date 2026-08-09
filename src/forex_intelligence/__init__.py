4e336e923db96ecb7fd86294e15d12c95c813753
orierodavid <orierodavid@yahoo.com>
2026-08-09 09:45:53 +0100

    feat: export anticipation engine

--- src/forex_intelligence/__init__.py
+++ src/forex_intelligence/__init__.py
@@ -1,3 +1,7 @@
 """Forex Market Intelligence platform."""
 
+from .anticipation import AnticipatedSetup, AnticipationEngine, AnticipationState
+
 __version__ = "0.1.0"
+
+__all__ = ["AnticipatedSetup", "AnticipationEngine", "AnticipationState"]
